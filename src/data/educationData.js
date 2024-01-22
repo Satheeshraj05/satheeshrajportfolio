46ef3ba453bbe75191deb8b042ab2eabe897a439
OHM NAMAH SHIVAYA <satheeshraj333@gmail.com>
2024-01-22 21:15:29 +0530

    deploy me portfolio

---
+++ src/data/educationData.js
@@ -0,0 +1,23 @@
+export const educationData = [
+    {
+        id: 1,
+        institution: 'AVB Matric Higher Secondary School',
+        course: 'Higher Secondary Education',
+        startYear: '2016',
+        endYear: '2018'
+    },
+    {
+        id: 2,
+        institution: 'Dr G R Damodaran College of Science',
+        course: 'Bachelor of Science',
+        startYear: '2018',
+        endYear: '2020'
+    },
+    {
+        id: 3,
+        institution: 'Amrita Vishwa Vidyapeetham',
+        course: 'Master Computer Application',
+        startYear: '2023',
+        endYear: 'Present'
+    },
+]
---
+++ src/data/experienceData.js
@@ -0,0 +1,23 @@
+export const experienceData = [
+    {
+        id: 1,
+        company: 'LUMOS IND',
+        jobtitle: 'Research Intern',
+        startYear: 'Aug, 2020 ',
+        endYear: 'Jan, 2021'
+    },
+    {
+        id: 2,
+        company: 'Edufied',
+        jobtitle: 'Frontend Developer Trainee',
+        startYear: '2024',
+        endYear: 'Present'
+    },
+    // {
+    //     id: 3,
+    //     company: 'Vehement Capital Partners',
+    //     jobtitle: 'Backend Developer',
+    //     startYear: '2019',
+    //     endYear: '2020'
+    // },
+]
